dfcf56fbdd9e42763dc2cac93ccd36958ccd2e13
Mathias Achleitner <mathias.achleitner@gmail.com>
2026-03-04 08:31:38 +0100

    -another attempt at fixing image and cv links

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 const repositoryName = "Portfolio_Website_2026";
 
-export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? `/${repositoryName}/` : "/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? `/${repositoryName}/` : "/",
   plugins: [react()],
-});
+}));
